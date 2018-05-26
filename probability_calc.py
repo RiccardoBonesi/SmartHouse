1be2c3b392e56d3f87288759aec8487e24da74ae
Stefano Carioni <phoenixxperia@gmail.com>
2018-05-26 17:06:56 +0200

    dati clcolati completamente

--- probability_calc.py
+++ probability_calc.py
@@ -25,10 +25,14 @@ def get_trans_prob(dataset):
     activities = dataset['Activity'].unique().tolist()
     matrix = np.zeros(len(activities))
     for state in activities:
+        #INDICI IN CUI è PRESENTE STATE
         wooo = [i for i, j in enumerate(dataset['Activity'].tolist()) if j == state]
+        #INDICI DELLO STATO SUCCESSIVO A STATE
         nextIndex = [x + 1 for x in wooo]
+        #QWER CONTIENE IL PRIMO INDEXOUTOFBOUND
         if qwer in nextIndex:
             nextIndex.remove(qwer)
+        #T CONTIENE GLI STATI SUCCESSIVI ALLO STATO CHE STO ANALIZZANDO (ES. TUTTI GLI STATI POST BREAKFAST)
         T = [dataset['Activity'].tolist()[i] for i in nextIndex]
         nextState.append(T)
         counter = []
@@ -48,23 +52,56 @@ def get_trans_prob(dataset):
 
 # calcola le probabilità delle osservazioni
 def get_obs_prob(dataset):
-
-    #data = pd.read_csv(dataset, sep="\t")
-    # per ogni attività trovare tutte le evidenze e calcolare la probabilità per ogni stato
+    evidence = []
 
 
     activities = dataset['Activity'].unique().tolist()
+    evidenceList = dataset['Evidence'].unique().tolist()
+    matrix = np.zeros(len(evidenceList))
 
-    for activity in activities:
-        x = [j for i, j in enumerate(dataset['Evidence'].tolist()) if j == activity]
-        print(x)
-    #dataCount.append(data['Activity'].value_counts())
+    for state in activities:
+        # INDICI IN CUI è PRESENTE STATE
+        wooo = [i for i, j in enumerate(dataset['Activity'].tolist()) if j == state]
+        # INDICI DELLO STATO SUCCESSIVO A STATE
 
+        # QWER CONTIENE IL PRIMO INDEXOUTOFBOUND
 
-    s1 = dataset.groupby(['Activity', 'Evidence']).size()  # sommo tutte le occorrenze di attività-evidenza
-    s = sum(s1)     # numero totale di evidenze
-    norm = [float(i) / s for i in s1]
+        # T CONTIENE GLI STATI SUCCESSIVI ALLO STATO CHE STO ANALIZZANDO (ES. TUTTI GLI STATI POST BREAKFAST)
+        T = [dataset['Evidence'].tolist()[i] for i in wooo]
+        evidence.append(T)
+        counter = []
+        for currentEvidence in evidenceList:
+            num = T.count(currentEvidence)
+            counter.append(num)
+            app = np.array(counter)
 
-    print("probabilità delle osservazioni calcolate")
-    return norm
+        norm = [float(i) / sum(app) for i in app]
+        matrix = numpy.vstack([matrix, norm])
+
+    matrix = numpy.delete(matrix, (0), axis=0)
+    print("probabilità di trasizione calcolate")
+    return matrix
+
+    # #data = pd.read_csv(dataset, sep="\t")
+    # # per ogni attività trovare tutte le evidenze e calcolare la probabilità per ogni stato
+    #
+    #
+    # activities = dataset['Activity'].unique().tolist()
+    #
+    # for activity in activities:
+    #     x = dataset.loc[dataset['Activity'] == activity]
+    #     s = sum(x['Evidence'].value_counts())
+    #     print(s)
+    #     norm = [float(i) / s for i in x['Evidence'].value_counts()]
+    #     print(norm)
+    # #dataCount.append(data['Activity'].value_counts())
+    #
+    #
+    # s1 = dataset.groupby(['Activity', 'Evidence']).size()  # sommo tutte le occorrenze di attività-evidenza
+    # s = sum(s1)     # numero totale di evidenze
+    # norm = [float(i) / s for i in s1]
+    #
+    # print("probabilità delle osservazioni calcolate")
+    # return norm
+
 
