49b2f6027e79ea75470a1f8ff8cc13ce733275f9
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-05-30 10:19:06 +0200

    aggiunto viterbi

--- hidden_markov_model.py
+++ hidden_markov_model.py
@@ -1,34 +1,57 @@
-from pomegranate import *
-from bayespy.nodes import Categorical, Mixture
-from bayespy.inference import VB
-from bayespy.nodes import CategoricalMarkovChain
-import bayespy.plot as bpplt
-
-def build_hmm(start_prob,trans_prob, obs_prob, merged_dataset):
-    asd = start_prob.values[0]
-    asd2 = trans_prob.values
-    asd3 = obs_prob.values
-    Z = CategoricalMarkovChain(start_prob.values[0], trans_prob.values, states=len(merged_dataset))
-    Y = Mixture(Z, Categorical, obs_prob.values)
-    states = Z.random()
-    observation = Mixture(states, Categorical, obs_prob.values).random()
-    evidences = merged_dataset['Evidence'].unique().tolist()
-
-    num_evidence = []
-
-    for i in range(len(merged_dataset)):
-        ev = merged_dataset['Evidence'][i]
-        index = evidences.index(ev)
-        num_evidence.append(index)
+import numpy as np
 
 
-    Y.observe(num_evidence)
-    Q = VB(Y, Z)
-    asd5 = Q.update()
-    print("ciao")
+class HMM:
+    #constructor
+    #transition_probs[i, j] is the probability of transitioning to state i from state j
+    #emission_probs[i, j] is the probability of emitting emission j while in state i
+    def __init__(self, transition_probs, emission_probs):
+        self._transition_probs = transition_probs
+        self._emission_probs = emission_probs
+
+    #accessors
+    def emission_dist(self, emission):
+        return self._emission_probs[:, emission]
+
+    @property
+    def num_states(self):
+        return self._transition_probs.shape[0]
+
+    @property
+    def transition_probs(self):
+        return self._transition_probs
 
 
+#the Viterbi algorithm
+def viterbi(hmm, initial_dist, emissions):
+    probs = hmm.emission_dist(emissions[0]) * initial_dist
+    stack = []
+
+    for emission in emissions[1:]:
+        trans_probs = hmm.transition_probs * np.row_stack(probs)
+        max_col_ixs = np.argmax(trans_probs, axis=0)
+        probs = hmm.emission_dist(emission) * trans_probs[max_col_ixs, np.arange(hmm.num_states)]
+
+        stack.append(max_col_ixs)
+
+    state_seq = [np.argmax(probs)]
+
+    while stack:
+        max_col_ixs = stack.pop()
+        state_seq.append(max_col_ixs[state_seq[-1]])
+
+    state_seq.reverse()
+
+    return state_seq
 
 
+#examples
+#from Wikipedia
+wiki_transition_probs = np.array([[0.7, 0.4], [0.3, 0.6]]) #0=Healthy, 1=Fever
+wiki_emissions = [2, 1, 0]
+wiki_emission_probs = np.array([[0.1, 0.4, 0.5], [0.6, 0.3, 0.1]]) #0=Dizzy, 1=Cold, 2=Normal
+wiki_initial_dist = np.array([[0.6, 0.4]])
+wiki_hmm = HMM(wiki_transition_probs, wiki_emission_probs)
 
-# <class 'list'>: ['BedPressureBedroom', 'CabinetMagneticBathroom', 'BasinPIRBathroom', 'ToiletFlushBathroom', 'ShowerPIRBathroom', 'FridgeMagneticKitchen', 'CupboardMagneticKitchen', 'ToasterElectricKitchen', 'CooktopPIRKitchen', 'MicrowaveElectricKitchen', 'SeatPressureLiving', 'MaindoorMagneticEntrance']
+if __name__ == "__main__":
+    print(viterbi(wiki_hmm, wiki_initial_dist, wiki_emissions))
--- main.py
+++ main.py
@@ -13,7 +13,30 @@ if __name__ == '__main__':
         startProb = get_start_prob(mergedDataset)
         transProb = get_trans_prob(mergedDataset)
         obsProb = get_obs_prob(mergedDataset) # passare mergedDataset
-        build_hmm(startProb,transProb,obsProb,mergedDataset)
+        # build_hmm(startProb,transProb,obsProb,mergedDataset)
+
+
+
+
+
+        wiki_transition_probs = np.array([[0.7, 0.4], [0.3, 0.6]])  # 0=Healthy, 1=Fever
+        wiki_emissions = [2, 1, 0]
+        wiki_emission_probs = np.array([[0.1, 0.4, 0.5], [0.6, 0.3, 0.1]])  # 0=Dizzy, 1=Cold, 2=Normal
+        wiki_initial_dist = np.array([[0.6, 0.4]])
+        wiki_hmm = HMM(wiki_transition_probs, wiki_emission_probs)
+
+        # if __name__ == "__main__":
+        print(viterbi(wiki_hmm, wiki_initial_dist, wiki_emissions))
+
+
+
+
+
+
+
+
+
+
         print("calcolate tutte le probabilità per il dataset {}".format(datasetList[datasetList.index(dataset)]))
 
 
